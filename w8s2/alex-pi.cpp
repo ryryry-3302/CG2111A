8360ef7cc0080daf97e5a8d6c2a1430a4c3df2a2
ryan leong <rjleong2002@gmail.com>
2024-04-03 11:44:31 +0800

    stuff

--- w8s2/alex-pi.cpp
+++ w8s2/alex-pi.cpp
@@ -284,7 +284,7 @@ int main()
 	while(!exitFlag)
 	{
 		char ch;
-		printf("Command (f=forward, b=reverse, l=turn left, r=turn right, s=stop, c=clear stats, g=get stats q=exit)\n");
+		printf("Command (f=forward, b=reverse, l=turn left, r=turn right, s=stop, c=clear stats, g=get stats, i = identify object q=exit)\n");
 		scanf("%c", &ch);
 
 		// Purge extraneous characters from input stream
